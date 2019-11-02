0620cb85cd9712dbfb78661d5ec2b6b9471bc7ee
Thu Nguyen <hoaithu7604@gmail.com>
2019-11-02 15:10:34 +0700

    create project structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import { StyleSheet,View,Text} from 'react-native';
+const styles = StyleSheet.create({
+  App:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  Title:{
+    fontSize:50,
+    color: '#1278AA'
+  }
+});
+export default class App extends Component 
+{
+  render()
+  {
+    return( 
+    <View style={styles.App}>
+      <Text style={styles.Title}>Weather Forecast</Text>
+    </View>
+    );
+  }
+}
